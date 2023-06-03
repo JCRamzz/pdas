15992993d2781e1469d6e08b1590dc40d13e5299
jcramz <jcramz1992@gmail.com>
2023-06-03 01:04:50 -0500

    final commit

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,20 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+function ProfileCard({ title, handle, image, description }) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p class="title is-4">{title}</p>
+          <p class="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
